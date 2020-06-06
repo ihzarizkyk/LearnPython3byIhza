1aa2dc7cc0e59306deadcec589c2b5ed7255822e
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-06-06 08:48:19 +0700

    Add files via upload

---
+++ pomodoro.py
@@ -0,0 +1,25 @@
+import time
+
+def pomodoro(mulai_mnt,mulai_dtk):
+# total detik 
+	total_dtk = mulai_mnt * 60 + mulai_dtk
+
+	while(total_dtk):
+
+	'''
+	fungsi dari divmod untuk membagi jumlah menit dengan 60 detik
+	dan menyimpan sisa bagi
+
+	02d artinya tampilkan 2 digit nomor dari menit dan detik
+	'''
+		mins, secs = divmod(total_dtk, 60)
+		print(f'{mins:02d}:{secs:02d}', end='\r')
+	'''
+	fungsi sleep untuk menunda waktu eksekusi
+	'''
+		time.sleep(1)
+		total_dtk -= 1
+	print("----POMODORO SELESAI----")
+
+if(__name__  == '__main__'):
+	pomodoro(25,00)
