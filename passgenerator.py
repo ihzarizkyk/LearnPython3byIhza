687c9f59c0ac34257991cd92b4316003d9570c71
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-06-30 06:40:01 +0700

    Add files via upload

---
+++ passgenerator.py
@@ -0,0 +1,10 @@
+import random
+
+userpass = input("Masukkan Password : ")
+simbol = "[]{}()*/-+"
+nomor = "1234567890"
+semua = userpass + simbol + nomor 
+
+panjang = 12
+password = "".join(random.sample(semua, panjang))
+print("HASIL PASSWORD GENERATOR : ",password)
