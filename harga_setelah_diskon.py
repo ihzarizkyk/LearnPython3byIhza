51f20e521e09acacd49f5dfbbe06e6736800cb13
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-07-12 10:38:26 +0700

    Delete harga_setelah_diskon.py

--- harga_setelah_diskon.py
+++
@@ -1,8 +0,0 @@
-'''
-Author : Mochammad Ihza Rizky Karim
-'''
-
-harga_asli = int(input("Masukkan Harga Barang : "))
-potongan = 20/100 * harga_asli
-harga = round(harga_asli-potongan)
-print("Harga Barang Setelah Diskon adalah ",harga)
