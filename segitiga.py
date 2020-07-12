02ab6744aef1bb16e4d2b95e0f2be4e13a4b4557
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-07-12 10:50:43 +0700

    Delete segitiga.py

--- segitiga.py
+++
@@ -1,6 +0,0 @@
-alas = int(input("Masukkan Alas : "))
-tinggi = int(input("Masukkan Tinggi : "))
-luas = 1/2*alas*tinggi
-
-print("Luas Segitiga Sama Kaki adalah ",luas)
-print("Perhitungan Selesai")
