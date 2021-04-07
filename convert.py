b728226dd85e6b52b366b225331a4e89b15fcd0e
Mochammad Ihza Rizky Karim <ihzarizky30@gmail.com>
2021-04-07 21:13:52 +0700

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,6 @@
+user = int(input("Masukkan Angka Desimal : "))
+
+print("Hasil Konversi :")
+print("Bilangan Biner dari ",user," adalah",bin(user))
+print("Bilangan Octal dari ",user," adalah",oct(user))
+print("Bilangan Biner dari ",user," adalah",hex(user))
